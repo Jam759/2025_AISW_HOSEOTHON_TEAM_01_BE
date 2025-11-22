cd6341f9480d5f158508a2896470571cb12b6a7c
Jam759 <jemin6372@naver.com>
2025-11-22 18:33:37 +0900

    init

---
+++ src/main/java/com/_oormthonUNIV/newnew/NewnewApplication.java
@@ -0,0 +1,13 @@
+package com._oormthonUNIV.newnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewnewApplication.class, args);
+	}
+
+}
